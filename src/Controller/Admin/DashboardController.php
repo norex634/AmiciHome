e05cd5bd74cf192c0803305353d0efdd0a774382
moule <norex.flo@gmail.com>
2023-06-27 14:32:25 +0200

    granted

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,20 +5,20 @@ namespace App\Controller\Admin;
 use App\Entity\Spe;
 use App\Entity\User;
 use App\Entity\Apply;
+use App\Entity\Article;
+use App\Entity\Category;
 use App\Entity\Image;
 use App\Entity\Classe;
-use App\Entity\Article;
 use App\Entity\Comment;
 use App\Entity\SpeRole;
-use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -79,9 +79,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::subMenu('Comment', 'fas fa-comment')->setSubItems([
             MenuItem::linkToCrud('Toutes les Commentaires','fas fa-user-comment',Comment::class)
         ]);
-        // yield MenuItem::subMenu('Catégories', 'fas fa-list')->setSubItems([
-        //     MenuItem::linkToCrud('Toutes les catégories','fas fa-user-comment',Category::class)
-        // ]);
 
         yield MenuItem::subMenu('News', 'fas fa-newspaper')->setSubItems([
             MenuItem::linkToCrud('Toutes les news','fas fa-newspaper',Article::class),
--- src/Controller/ApplyController.php
+++ src/Controller/ApplyController.php
@@ -53,6 +53,7 @@ class ApplyController extends AbstractController
 
     #[Route('/apply', name: 'ApplyShowAll')]
     #[IsGranted("ROLE_ROOSTER", message: "Vous n'avez pas le droit d'accéder à cette ressource")]
+    #[IsGranted("ROLE_ADMIN")]
     public function showAll(ApplyRepository $applyRepo): Response
     {
         $apply = $applyRepo->findAll();
